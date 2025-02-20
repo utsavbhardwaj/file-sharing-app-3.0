13dc5de8a4aaac38cec152d539506614c35a3e87
Utsav <133682780+utsavbhardwaj@users.noreply.github.com>
2025-02-21 01:40:21 +0530

    Update UploadForm.js file size
    upgraded the initial 2 MB file size limit to 20MB for all the users

--- app/(dashboard)/(routes)/upload/_components/UploadForm.js
+++ app/(dashboard)/(routes)/upload/_components/UploadForm.js
@@ -8,9 +8,9 @@ function UploadForm({uploadBtnClick,progress}) {
     const [errorMsg, setErrorMsg] = useState();
     const onFileSelect = (file) => {
         console.log(file)
-        if (file && file.size > 2000000) {
-            console.log("Size is Greate than 2 MB");
-            setErrorMsg('Maximum File Upload Size is 2MB')
+        if (file && file.size > 20000000) {
+            console.log("Size is Greate than 20 MB");
+            setErrorMsg('Maximum File Upload Size is 20MB')
             return;
         }
         setErrorMsg(null)
@@ -40,7 +40,7 @@ function UploadForm({uploadBtnClick,progress}) {
                                 Click to upload</span>
                           or  <strong className='text-primary'>
                                 drag</strong> and <strong className='text-primary'>drop</strong></p>
-                        <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF ( Max Size : 2MB)</p>
+                        <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or PDF ( Max Size : 20MB)</p>
                     </div>
                     <input id="dropzone-file"
                         type="file" className="hidden"
